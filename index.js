2c5d68bc1bdd42ed8e83906f2e8518d96e2bb8bd
Colin Luzio <blissful1980@gmail.com>
2019-01-27 20:06:10 +0000

    add mouse event handlers

--- index.js
+++ index.js
@@ -31,12 +31,33 @@ export default class DragDropReorder extends Component{
     return undefined;
   }
 
+  initializeDrag(event){
+    event.preventDefault();
+
+    // Mouse events
+    window.addEventListener('mousemove', this.onMouseMove); // Move mouse
+    window.addEventListener('mouseup', this.onMouseUp); // Mouse up
+  }
+
+  onMouseMove(event){
+    event.preventDefault();
+  }
+
+  onMouseUp(event){
+    event.preventDefault();
+
+    // Mouse events
+    window.removeEventListener('mouseup', this.onMouseUp); // Mouse up
+    window.removeEventListener('mousemove', this.onMouseMove); // Mouse move
+
+  }
+
   render(){
     let self = this;
     let list = this.state.list.map(function (item, index) {
                   let itemKey = item[self.props.itemKey] || item;
                   let itemClass = [self.props.itemClass, self.getPlaceholderClass(item), self.getSelectedClass(item)].join(' ');
-                  return <div key={itemKey} className={itemClass}>Hello</div>
+                  return <div key={itemKey} className={itemClass} onMouseDown={self.initializeDrag.bind(self)}>Hello</div>
                });
 
     return(
