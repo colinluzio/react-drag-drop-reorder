dba0b9f1c8d362452065fddc648f76ec46a6f858
Colin Luzio <blissful1980@gmail.com>
2019-01-27 17:19:00 +0000

    add method to get class of element

--- index.js
+++ index.js
@@ -8,16 +8,46 @@ export default class DragDropReorder extends Component{
     super(props)
 
     this.state = {
-
+      list :     props.list,
+      itemClass: props.itemClass,
+      itemKey:   props.itemKey
     }
   }
+
+  getPlaceholderClass (item) {
+    if (this.state.held && this.state.dragged && this.state.dragged.item === item) {
+      return 'placeholder';
+    }
+    return undefined;
+  }
+
+  getSelectedClass (item) {
+    if (typeof this.props.selected !== 'undefined') {
+      if (typeof this.props.selectedKey !== 'undefined') {
+        return this.props.selected[this.props.selectedKey] === item[this.props.selectedKey] ? 'selected' : undefined;
+      }
+      return this.props.selected === item ? 'selected' : undefined;
+    }
+    return undefined;
+  }
+
   render(){
+    let self = this;
+    let list = this.state.list.map(function (item, index) {
+                  let itemKey = item[self.props.itemKey] || item;
+                  let itemClass = [self.props.itemClass, self.getPlaceholderClass(item), self.getSelectedClass(item)].join(' ');
+                  return <div key={itemKey} className={itemClass}>Hello</div>
+               });
+
     return(
-      <div></div>
+      <div className={this.props.listClass}>
+        {list}
+      </div>
     );
   }
 }
 
 DragDropReorder.propTypes = {
-    items: PropTypes.array.isRequired
+    list: PropTypes.array.isRequired,
+    listClass: PropTypes.string.isRequired,
 }
